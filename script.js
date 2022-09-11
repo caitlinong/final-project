284996285ed22748984e7fa7ca615ea4276eeef4
Caitlin Ong <caitlinong@Caitlins-MacBook-Pro.local>
2022-09-11 16:21:36 +1000

    Updated files

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// ENLARGE TEXT ON HOVER
+
 document.getElementById("title").addEventListener("mouseover", function () {
   document.getElementById("title").style.fontSize = "120%";
 });
@@ -6,11 +8,15 @@ document.getElementById("title").addEventListener("mouseout", function () {
   document.getElementById("title").style.fontSize = "initial";
 });
 
+// ADVICE SLIP (not working)
+
 let response = $.ajax("https://api.adviceslip.com/advice");
 console.log(response);
 
 $.ajax("https://api.adviceslip.com/advice", {
   success: function (APIResponse) {
+    console.log(APIResponse);
+
     let adviceTag = document.createElement("p");
 
     adviceTag.innerHTML = APIResponse;
